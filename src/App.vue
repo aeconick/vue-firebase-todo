6d21e94bb93c6cfb22b75da1a4e47b24e72d8d95
Nikolay Cherepov <aeconick@gmail.com>
2023-09-05 19:46:22 +0300

    Add static template

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,52 @@
 <template>
-  todos nick dasdsad
+  <div class="todo-app-container">
+
+    <div class="title has-text-centered">
+      Vue Firebase Todo App
+    </div>
+
+    <div class="field is-grouped mb-5">
+      <p class="control is-expanded">
+        <input class="input" type="text" placeholder="Add a todo">
+      </p>
+      <p class="control">
+        <a class="button is-info">
+          Add
+        </a>
+      </p>
+    </div>
+
+    <div v-for="i in 3" class="card mb-5">
+      <div class="card-content">
+        <div class="content">
+
+          <div class="columns is-mobile is-vcentered">
+            <div class="column">
+              Feed the dog.
+            </div>
+            <div class="column is-5 has-text-right">
+              <button class="button is-light">
+                &check;
+              </button>
+              <button class="button is-danger ml-2">
+                &cross;
+              </button>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+  </div>
 </template>
+
+<style>
+@import 'bulma/css/bulma.min.css';
+
+.todo-app-container {
+  max-width: 400px;
+  padding: 20px;
+  margin: 0 auto;
+}
+</style>
