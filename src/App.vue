8e783cbfa88495bbf80da9093ca2cf3f526b093c
Nikolay Cherepov <aeconick@gmail.com>
2023-09-06 11:13:16 +0300

    Add todos from input

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,18 @@
       Vue Firebase Todo App
     </div>
 
-    <div class="field is-grouped mb-5">
-      <p class="control is-expanded">
-        <input class="input" type="text" placeholder="Add a todo">
-      </p>
-      <p class="control">
-        <a class="button is-info">
-          Add
-        </a>
-      </p>
-    </div>
+    <form @submit.prevent="addTodo">
+      <div class="field is-grouped mb-5">
+        <p class="control is-expanded">
+          <input v-model="newTodoContent" class="input" type="text" placeholder="Add a todo">
+        </p>
+        <p class="control">
+          <button :disabled="!newTodoContent" class="button is-info">
+            Add
+          </button>
+        </p>
+      </div>
+    </form>
 
     <div v-for="todo in todos" class="card mb-5">
       <div class="card-content">
@@ -43,19 +45,33 @@
 
 <script setup>
 import { ref } from 'vue';
+import { v4 as uuidv4 } from 'uuid';
 
 const todos = ref([
-  {
-    id: '1',
-    content: 'Feed the dog',
-    done: false,
-  },
-  {
-    id: '2',
-    content: 'Feed the cat',
+  // {
+  //   id: '1',
+  //   content: 'Feed the dog',
+  //   done: false,
+  // },
+  // {
+  //   id: '2',
+  //   content: 'Feed the cat',
+  //   done: false,
+  // }
+]);
+
+const newTodoContent = ref('');
+
+const addTodo = () => {
+  const newTodo = {
+    id: uuidv4(),
+    content: newTodoContent.value,
     done: false,
   }
-])
+  todos.value.unshift(newTodo);
+  newTodoContent.value = '';
+}
+
 </script>
 
 <style>
