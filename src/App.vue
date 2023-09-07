ba2d20736db98f47e8bbd7257aa90b410d93d83b
Nikolay Cherepov <aeconick@gmail.com>
2023-09-07 22:11:27 +0300

    Delete todo from firebase

--- src/App.vue
+++ src/App.vue
@@ -45,7 +45,7 @@
 
 <script setup>
 import { ref, onMounted } from 'vue';
-import { collection, onSnapshot, addDoc } from 'firebase/firestore';
+import { collection, onSnapshot, addDoc, deleteDoc, doc } from 'firebase/firestore';
 import { db } from '@/firebase';
 
 const todosCollectionReff = collection(db, 'todos');
@@ -90,7 +90,7 @@ const addTodo = () => {
 };
 
 const deleteTodo = (id) => {
-  todos.value = todos.value.filter(todo => todo.id !== id);
+  deleteDoc(doc(todosCollectionReff, id));
 };
 
 const toggleDone = (id) => {
